f2345199d06d8af29db19ed84c6dd062013bec46
liuyufei <gabriel.liu3615@gmail.com>
2017-07-22 01:19:15 +1200

    bible deuteronomy

--- js/custom.js
+++ js/custom.js
@@ -8,89 +8,43 @@ $(function () { // wait for document ready
         triggerElement: '#panel-1',
         // triggerHook: 0.9,
         duration: "150%"
-    })
-        // .addIndicators({
-        //     name: 'can help scene',
-        //     colorTrigger: 'black',
-        //     indent: 10
-        // })
-        .setClassToggle('#can_help', 'fade-in').addTo(fadeinController);
+    }).setClassToggle('#can_help', 'fade-in').addTo(fadeinController);
 
     //about pop-up scene
     new ScrollMagic.Scene({
         triggerElement: '#panel-2',
         // triggerHook: 0.9,
         duration: "150%"
-    })
-    // .addIndicators({
-    //     name: 'can help scene',
-    //     colorTrigger: 'black',
-    //     indent: 10
-    // })
-        .setClassToggle('#about_life', 'left-in').addTo(fadeinController);
+    }).setClassToggle('#about_life', 'left-in').addTo(fadeinController);
 
 
+    new ScrollMagic.Scene({
+        triggerElement: '#panel-4',
+        triggerHook: 0.3,
+        duration: "100%"
+    }).setClassToggle('#bible-deuteronomy', 'fade-in').addTo(fadeinController);
 
-    // $('.panel').each(function () {
-    //     //first panel fade in scene
-    //     new ScrollMagic.Scene({
-    //         triggerElement: this,
-    //         reverse:false,
-    //         triggerHook: 0.9,
-    //         duration: "100%"
-    //     }).setClassToggle('.show_all', 'fade-in')
-    //         .addIndicators({
-    //             name: 'panel fade scene',
-    //             colorTrigger: 'black',
-    //             indent: 100
-    //         }) // add indicators (requires plugin)
-    //         .addTo(fadeinController);
-    // })
 
     new ScrollMagic.Scene({
         triggerElement: '#panel-1',
         duration: "100%"
-    }).setClassToggle('body', 'moss')
-        // .addIndicators({
-        //     name: 'panel 1 scene',
-        //     colorTrigger: 'black',
-        //     indent: 100
-        // })
-        .addTo(fadeinController);
+    }).setClassToggle('body', 'moss').addTo(fadeinController);
 
 
     new ScrollMagic.Scene({
         triggerElement: '#panel-2',
         duration: "100%"
-    }).setClassToggle('body', 'waterfall')
-        // .addIndicators({
-        //     name: 'panel 2 scene',
-        //     colorTrigger: 'black',
-        //     indent: 100
-        // })
-        .addTo(fadeinController);
+    }).setClassToggle('body', 'waterfall').addTo(fadeinController);
 
     new ScrollMagic.Scene({
         triggerElement: '#panel-3',
         duration: "100%"
-    }).setClassToggle('body', 'meadow')
-        // .addIndicators({
-        //     name: 'panel 3 scene',
-        //     colorTrigger: 'black',
-        //     indent: 100
-        // })
-        .addTo(fadeinController);
+    }).setClassToggle('body', 'meadow').addTo(fadeinController);
 
     new ScrollMagic.Scene({
         triggerElement: '#panel-4',
         duration: "100%"
-    }).setClassToggle('body', 'thundercloud')
-        // .addIndicators({
-        //     name: 'panel 4 scene',
-        //     colorTrigger: 'black',
-        //     indent: 100
-        // })
-        .addTo(fadeinController);
+    }).setClassToggle('body', 'thundercloud').addTo(fadeinController);
 
     //parallax scene
     new ScrollMagic.Scene({
@@ -98,11 +52,12 @@ $(function () { // wait for document ready
         triggerHook:1,
         duration: "150%"
     }).setTween(TweenMax.from('.bcg',1,{y:'-50%',ease:Power0.easeNone}))
-        .addIndicators({
-        name: 'parallax scene',
-        colorTrigger: 'black',
-        indent: 100
-    }).addTo(fadeinController)
+        // .addIndicators({
+        // name: 'parallax scene',
+        // colorTrigger: 'black',
+        // indent: 100
+        // })
+        .addTo(fadeinController)
 
 
 });
