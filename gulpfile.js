7e9a835ab88af5e8a16ad73be40c97660f51e8e3
liuyufei <gabriel.liu3615@gmail.com>
2017-07-27 00:45:36 +1200

    min css&js

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+//script paths
+var jsFiles = ['js/custom.js','js/library.js']
+    jsDest = 'dist/js';
+
+gulp.task('scripts', function() {
+    return gulp.src(jsFiles)
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest(jsDest))
+        .pipe(rename('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('css', function () {
+    gulp.src('css/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'));
+});
